f1d3f53b88e22e07be8146428e88ffbc3a56243b
jeremyvalette <jeremy.valette@etu.univ-smb.fr>
2017-05-10 17:23:21 +0200

    Delete camera.php

--- onebeeV1/camera.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Permet d'afficher le flux vidéo de motion sur une page web.
-	// N'oubliez pas de configurer l'adresse IP et le port correctement.
-
-	<img alt="http://127.0.0.1:8081/" src="http://127.0.0.1:8081/">
-
-?>
