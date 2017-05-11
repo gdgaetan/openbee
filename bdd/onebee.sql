a0831fe651f16ef0571c0f1ec82fee3665d465cc
jeremyvalette <jeremy.valette@etu.univ-smb.fr>
2017-05-11 10:14:50 +0200

    Add files via upload

---
+++ bdd/onebee.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mar 25 Avril 2017 à 08:59
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `onebee`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `abeille`
+--
+
+CREATE TABLE IF NOT EXISTS `abeille` (
+  `idAbeille` int(11) NOT NULL AUTO_INCREMENT,
+  `dateEnregistrement` datetime NOT NULL,
+  `nbEntreesAbeille` int(11) NOT NULL,
+  `nbSortiesAbeille` int(11) NOT NULL,
+  `NBEFauxBourdon` int(11) NOT NULL,
+  `NBSFauxBourdon` int(11) NOT NULL,
+  `NBEAbeillePollen` int(11) NOT NULL,
+  `compteurEntree` bigint(20) NOT NULL,
+  `compteurSortie` bigint(20) NOT NULL,
+  PRIMARY KEY (`idAbeille`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+--
+-- Structure de la table `compteur`
+--
+
+CREATE TABLE IF NOT EXISTS `compteur` (
+  `idCompteur` int(11) NOT NULL,
+  `inTotal` bigint(20) NOT NULL,
+  `outTotal` bigint(20) NOT NULL,
+  PRIMARY KEY (`idCompteur`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
