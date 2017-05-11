a006a8b7ba630180b81bd36708f63f04f34c9740
orion40 <qolizion@protonmail.com>
2017-05-11 09:08:13 +0200

    Modification user:pass pour la BDD: onebee/onebee

--- onebee/bdd/connection.php
+++ onebee/bdd/connection.php
@@ -2,7 +2,7 @@
 	// connexion à la base de données
 	try
 	{
-		$bdd = new PDO('mysql:host=localhost;dbname=onebee', 'root', '');
+		$bdd = new PDO('mysql:host=localhost;dbname=onebee', 'onebee', 'onebee');
 	}
 	catch(Exception $e)
 	{
