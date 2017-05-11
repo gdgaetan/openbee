74f90c0e50f5c741a97e5467b703f4aa992fd47d
gaetan deprez <gdgaetan@gmail.com>
2017-05-11 10:43:07 +0200

    Add files via upload

--- onebeetest/compteur.php
+++ onebeetest/compteur.php
@@ -10,14 +10,50 @@ include 'connection.php';
 		$res = $requete->fetch();
 		$requete = $bdd->query ('SELECT * FROM abeille where dateEnregistrement <= "'.$_SESSION['debut'].'" order by dateEnregistrement desc');
 		$res2 = $requete->fetch();
-		echo 'Depuis le '.$_SESSION['debut'].'</br>';
-		echo 'Entrées : ' . ($res['inTotal']-$res2['compteurEntree']) . '</br>Sorties : ' . ($res['outTotal']-$res2['compteurSortie']) . '</br>Différence : '. (($res['inTotal']-$res2['compteurEntree']) - ($res['outTotal']-$res2['compteurSortie']));
+		echo '
+		<div class="container">
+		  <p>Depuis le '.$_SESSION['debut'].':</p>            
+		  <table class="table">
+			<thead>
+			  <tr>
+				<th>Entrées</th>
+				<th>Sorties</th>
+				<th>Différence</th>
+			  </tr>
+			</thead>
+			<tbody>
+			  <tr>
+				<td>' . ($res['inTotal']-$res2['compteurEntree']) . '</td>
+				<td>' . ($res['outTotal']-$res2['compteurSortie']) . '</td>
+				<td>'. (($res['inTotal']-$res2['compteurEntree']) - ($res['outTotal']-$res2['compteurSortie'])) . '</td>
+			  </tr>
+			</tbody>
+		  </table>
+		</div>';
 	}
 	else
 	{
 		$requete = $bdd->query ('SELECT * FROM compteur');
 		$res = $requete->fetch();
-		echo 'Entrées : ' . $res['inTotal'] . '</br>Sorties : ' . $res['outTotal'] . '</br>Différence : '. ($res['inTotal'] - $res['outTotal']);
+		echo'
+		<div class="container">          
+		  <table class="table">
+			<thead>
+			  <tr>
+				<th>Entrées</th>
+				<th>Sorties</th>
+				<th>Différence</th>
+			  </tr>
+			</thead>
+			<tbody>
+			  <tr>
+				<td>' . $res['inTotal'] . '</td>
+				<td>' . $res['outTotal'] . '</td>
+				<td>'. ($res['inTotal'] - $res['outTotal']) . '</td>
+			  </tr>
+			</tbody>
+		  </table>
+		</div>';
 	}
 
 	?>
