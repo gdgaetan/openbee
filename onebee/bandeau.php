9089f0eafb2f7a32ce6fff8bfeb6e0b2f3d4af67
orion40 <qolizion@protonmail.com>
2017-05-11 09:47:09 +0200

    supression repertoire onebee

--- onebee/bandeau.php
+++
@@ -1,6 +0,0 @@
-<div id="top">
-<span id="mind"><img src="image/mind.png" alt="mind" width="230" height="212"></span>
-<span id="onebee"><a href="index.php"><img src="image/onebee.png" alt="onebee" width="150" height="150"></a></span>
-<span id="univsavoie"><img src="image/univsavoie.png" alt="univsavoie" width="" height=""></span>
-<div id="titre"><h1>PROJET ONE BEE 2017</h1></div>
-</div>
