37befc1a6a17753847aea991bbcf11e7d9ec1874
gaetan deprez <gdgaetan@gmail.com>
2017-05-17 09:12:51 +0200

    Add files via upload

--- onebeetest/compteur.php
+++ onebeetest/compteur.php
@@ -1,60 +1,58 @@
 <?php 
-session_start();
-include 'connexion.php';
-?>
-<div id="compteur">
-	<?php
-	if ( isset($_SESSION['debut']))
-	{
-		$requete = $bdd->query ('SELECT * FROM compteur');
-		$res = $requete->fetch();
-		$requete = $bdd->query ('SELECT * FROM abeille where dateEnregistrement <= "'.$_SESSION['debut'].'" order by dateEnregistrement desc');
-		$res2 = $requete->fetch();
-		echo '
-		<div class="containerCompteur">
-		  <p>Depuis le '.$_SESSION['debut'].':</p>            
-		  <table class="table">
-			<thead>
-			  <tr>
-				<th>Entrées</th>
-				<th>Sorties</th>
-				<th>Différence</th>
-			  </tr>
-			</thead>
-			<tbody>
-			  <tr>
-				<td>' . ($res['inTotal']-$res2['compteurEntree']) . '</td>
-				<td>' . ($res['outTotal']-$res2['compteurSortie']) . '</td>
-				<td>'. (($res['inTotal']-$res2['compteurEntree']) - ($res['outTotal']-$res2['compteurSortie'])) . '</td>
-			  </tr>
-			</tbody>
-		  </table>
-		</div>';
-	}
-	else
-	{
-		$requete = $bdd->query ('SELECT * FROM compteur');
-		$res = $requete->fetch();
-		echo'
-		<div class="containerCompteur">          
-		  <table class="table">
-			<thead>
-			  <tr>
-				<th>Entrées</th>
-				<th>Sorties</th>
-				<th>Différence</th>
-			  </tr>
-			</thead>
-			<tbody>
-			  <tr>
-				<td>' . $res['inTotal'] . '</td>
-				<td>' . $res['outTotal'] . '</td>
-				<td>'. ($res['inTotal'] - $res['outTotal']) . '</td>
-			  </tr>
-			</tbody>
-		  </table>
-		</div>';
-	}
+session_start();/*on démare une session pour les varriables de session que l'on va avoir, ici la seule varrible de session que l'on a est la varrible "debut"
+dans laquelle on a la date(aaaa-mm-jjThh:mm) à laquelle le compteur doit démarrer*/
+include 'connexion.php';/*on ajoute le code qui nous permet de nous loguer à la bdd*/
+$requete = $bdd->query ('SELECT * FROM compteur');/*on selectionne tous ce que la table compteur contient, soit une ligne*/
+$res = $requete->fetch();/*et on met le résultat dans la varriable res*/
+if ( isset($_SESSION['debut']))/*si l'utilisateur a renseigné la date à laquelle sont comteur doit démarer*/
+{
+	$requete = $bdd->query ('SELECT * FROM abeille where dateEnregistrement <= "'.$_SESSION['debut'].'" order by dateEnregistrement desc');/*on choisit toutes les dates plus pettites ou égale à la date
+	renseigné et on met la date la plus résente en première*/
+	$res2 = $requete->fetch();/*on prend la première ligne ,soit la ligne avec la date la plus récente, et on l met dans la varrible res2*/
+	echo '
+	<div class="containerCompteur"> <!-- classe containerCompteur qui redimensionne, dans flux.css, le tableau créé par la mise en page de bootstrap  -->
+	  <p>Depuis le '.$_SESSION['debut'].':</p>   <!-- on affiche dapuis quelle date le comteur compte -->
+	  <!-- mise en page permettent d avoir un jolie tableau grâce à bootstrap -->
+	  <table class="table">
+		<thead>
+		  <tr>
+			<th>Entrées</th>
+			<th>Sorties</th>
+			<th>Différence</th>
+		  </tr>
+		</thead>
+		<tbody>
+		  <tr>
+			<td>' . ($res['inTotal']-$res2['compteurEntree']) . '</td> <!-- on affiche la différence entre le nombre total d abeille entrentre et le nombre d abeille entrente avant la date sélectionné -->
+			<td>' . ($res['outTotal']-$res2['compteurSortie']) . '</td></td> <!-- on affiche la différence entre le nombre total d abeille sortante et le nombre d abeille sortante avant la date sélectionné -->
+			<td>'. (($res['inTotal']-$res2['compteurEntree']) - ($res['outTotal']-$res2['compteurSortie'])) . '</td></td> <!-- on affiche la différence entre les 2 affichage précédent -->
+		  </tr>
+		</tbody>
+	  </table>
+	</div>';
+}
+else/*si l'utilisateur n'a pas renseigné de date*/
+{
+	echo'
+	<div class="containerCompteur"> <!-- classe containerCompteur qui redimensionne, dans flux.css, le tableau créé par la mise en page de bootstrap  -->
+	  <!-- mise en page permettent d avoir un jolie tableau grâce à bootstrap -->
+	  <table class="table">
+		<thead>
+		  <tr>
+			<th>Entrées</th>
+			<th>Sorties</th>
+			<th>Différence</th>
+		  </tr>
+		</thead>
+		<tbody>
+		  <tr>
+			<td>' . $res['inTotal'] . '</td> <!-- on affiche le nombre total d abeille entrente -->
+			<td>' . $res['outTotal'] . '</td> <!-- on affiche le nombre total d abeille sortante -->
+			<td>'. ($res['inTotal'] - $res['outTotal']) . '</td> <!-- on affiche la différence entre les 2 affichage précédent -->
+		  </tr>
+		</tbody>
+	  </table>
+	</div>';
+}
 
-	?>
-</div>
+?>
