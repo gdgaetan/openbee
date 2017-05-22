e555568e5451f0fd996e5c6cc891c2ce9a0f792a
pi <someone@test.fr>
2017-05-22 17:38:32 +0200

    correction USER §ls

--- onebeeV2/connexion.php
+++ onebeeV2/connexion.php
@@ -2,11 +2,7 @@
 	// connexion à la base de données
 	try
 	{
-<<<<<<< HEAD
 		$bdd = new PDO('mysql:host=localhost;dbname=comptage_video', 'root', '');
-=======
-		$bdd = new PDO('mysql:host=localhost;dbname=comptage_video', 'onebee', '');
->>>>>>> 1d50c5ba5d81dc3b40b685b1845eb37653fbcd15
 	}
 	catch(Exception $e)
 	{
