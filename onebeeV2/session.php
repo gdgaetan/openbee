e63e3e6503083b2143989138afb4bd903d03e7c7
SoleneDe <demarsolene@gmail.com>
2017-05-22 15:39:50 +0200

    bon fuseau horaire

--- onebeeV2/session.php
+++ onebeeV2/session.php
@@ -1,4 +1,5 @@
 <?php 
 session_start();/*on démare une session pour les varriables de session que l'on va avoir, ici la seule varrible de session que l'on a est la varrible "debut"*/
-$_SESSION['dateCourante'] = date("Y-m-d H:i");//varriable comtenant la date courante
+$_SESSION['dateCourante'] = new DateTime();//varriable comtenant la date courante
+$_SESSION['dateCourante']->setTimezone(new DateTimeZone("Europe/Paris")); // pour avoir la bonne heure
 ?>
